730fb41d011b0f203a70e5bdc2ee7d0f9f72a51d
Lucky-Nicke <glucky.nicke@gmail.com>
2025-05-23 17:22:05 +0800

    创建项目
    2025-2-23--17:22

---
+++ src/main/java/club/lanxige/smmm/SmmmApplication.java
@@ -0,0 +1,13 @@
+package club.lanxige.smmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/lanxige/smmm/SmmmApplicationTests.java
@@ -0,0 +1,13 @@
+package club.lanxige.smmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
